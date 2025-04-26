91cec19502a96e3f0e33cdcad4f86458307c0698
Muhammad Marza Ananda <137996356+marzaananda@users.noreply.github.com>
2025-04-26 16:31:10 +0700

    AI
    22nya baru

---
+++ Assets/App/Scenes/marza/own ai/Cover.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace GDL_tutorial
+{
+    public class Cover : MonoBehaviour
+    {
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.magenta;
+            Gizmos.DrawCube(transform.position, Vector3.one * 0.3f);
+        }
+    }
+
+}
+public class Cover
+{
+    
+}
